3fb559714ee056f9456262bf41745e6cd21cae47
ironoir <cohyou@ironoir.io>
2021-12-28 17:56:32 +0900

    making...

--- src/btree2.rs
+++ src/btree2.rs
@@ -1,14 +1,39 @@
-struct Page([u16; PAGE_SIZE]);
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io;
+use std::io::Read;
+use std::io::Seek;
+use std::io::SeekFrom;
+use std::io::Write;
+
+
+const PAGE_SIZE: usize = 64;
+
+
+struct Page([u8; PAGE_SIZE]);
 struct Node { page_id: u16, page: Page }
 struct RootNode(Node);
 struct BranchNode(Node);
 struct LeafNode(Node);
-struct BTree { next_page_id: u16, root: Node }
+struct BTree { next_page_id: u16, root: Node, file: File }
+
 
 impl BTree {
     pub fn new() -> Self {
         let root = Node::new(0);
-        BTree { next_page_id: 1, root: root }
+        let file = BTree::open_file();
+        let file_size = file.metadata().unwrap().len();
+        let next_page_id = file_size / PAGE_SIZE as u64;
+        BTree { next_page_id: next_page_id as u16, root: root, file: file }
+    }
+
+    fn open_file() -> File {
+        let file_path = "f";
+        OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(file_path).unwrap()
     }
 }
 
@@ -16,51 +41,25 @@ impl Node {
     fn new(page_id: u16) -> Self {
         Node { page_id: page_id, page: Page::new() }
     }
+
+    fn read(&mut self, mut file: File) -> io::Result<()> {
+        let offset = PAGE_SIZE as u64 * self.page_id as u64;
+        file.seek(SeekFrom::Start(offset))?;
+        file.read_exact(&mut self.page.0)
+    }
+
+    fn write(&mut self, mut file: File) -> io::Result<()> {
+        let offset = PAGE_SIZE as u64 * self.page_id as u64;
+        file.seek(SeekFrom::Start(offset))?;
+        file.write_all(&self.page.0)
+    }
 }
 
 impl Page {
     fn new() -> Self {
         Page([0; PAGE_SIZE])
     }
-
-    fn write(page_id: u16) {
-        let file_name = "f";
-        let mut file = OpenOptions::new().write(true).create(true).open(file_name)?;
-        file.write_all(self.0.as_ref())
-    }
 }
-
-const PAGE_SIZE: usize = 64;
-
-// pub fn new(heap_file: File) -> io::Result<Self> {
-//     let heap_file_size = heap_file.metadata()?.len();
-//     let next_page_id = heap_file_size / PAGE_SIZE as u64;
-//     Ok(Self {
-//         heap_file,
-//         next_page_id,
-//     })
-// }
-
-// pub fn open(heap_file_path: impl AsRef<Path>) -> io::Result<Self> {
-//     let heap_file = OpenOptions::new()
-//         .read(true)
-//         .write(true)
-//         .create(true)
-//         .open(heap_file_path)?;
-//     Self::new(heap_file)
-// }
-
-// pub fn read_page_data(&mut self, page_id: PageId, data: &mut [u8]) -> io::Result<()> {
-//     let offset = PAGE_SIZE as u64 * page_id.to_u64();
-//     self.heap_file.seek(SeekFrom::Start(offset))?;
-//     self.heap_file.read_exact(data)
-// }
-
-// pub fn write_page_data(&mut self, page_id: PageId, data: &[u8]) -> io::Result<()> {
-//     let offset = PAGE_SIZE as u64 * page_id.to_u64();
-//     self.heap_file.seek(SeekFrom::Start(offset))?;
-//     self.heap_file.write_all(data)
-// }
 
 // pub fn allocate_page(&mut self) -> PageId {
 //     let page_id = self.next_page_id;
